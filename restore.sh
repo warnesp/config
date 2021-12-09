e62239ee799f36a9aedef3b78935e8f759d7d1ca
Paul Warnes <pwarnes@gmail.com>
2021-12-09 12:35:12 -0600

    add doxygen plugin, create pluging restore function

--- restore.sh
+++ restore.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 [ -f /etc/redhat-release ] && 
-  echo "restoring for RHEL"
+echo "restoring for RHEL"
+
+
+function RestorePlugin() 
+{
+    local pluginName="$1"
+    local pluginUrl="$2"
+
+    mkdir -p "$HOME/.vim/pack/vendor/start/$pluginName" &&
+        rm -rf "$HOME/.vim/pack/vendor/start/$pluginName" && 
+    git clone --depth 1 -q $pluginUrl $HOME/.vim/pack/vendor/start/$pluginName || 
+    echo "Failed to get $pluginName"
+
+    [ -f /etc/redhat-release ] && 
+    echo "Installing $pluginName for RHEL" && 
+    mkdir -p "$HOME/.vim/plugins/$pluginName" && 
+    rm -rf "$HOME/.vim/plugins/$pluginName" && 
+    cp -r "$HOME/.vim/pack/vendor/start/$pluginName" "$HOME/.vim/plugins/$pluginName" 
+
+
+}
 
 
 ##restore config
@@ -11,71 +31,59 @@ mkdir -p $HOME/shortcuts/bin
 #i3 config
 if command -v i3 > /dev/null
 then
-  echo "Restoring i3 settings"
+    echo "Restoring i3 settings"
 
-  mkdir -p "$HOME/.fonts" && cp -r home/fonts/* $HOME/.fonts/
-  cp -r i3status $HOME/.config/
-  cp -r i3 $HOME/.config/
+    mkdir -p "$HOME/.fonts" && cp -r home/fonts/* $HOME/.fonts/
+    cp -r i3status $HOME/.config/
+    cp -r i3 $HOME/.config/
 else
-  echo "No i3"
+    echo "No i3"
 fi
 
 #vim
 if command -v vim > /dev/null 
 then
-  echo "Restoring VIM settings"
-  cp $HOME/config/vim/vimrc $HOME/.vimrc
+    echo "Restoring VIM settings"
+    cp $HOME/config/vim/vimrc $HOME/.vimrc
 
-  # setup alternate swap location
-  mkdir -p $HOME/.vim/swapfiles
-  mkdir -p $HOME/.vim/backups
+    # setup alternate swap location
+    mkdir -p $HOME/.vim/swapfiles
+    mkdir -p $HOME/.vim/backups
 
 
-  if command -v git > /dev/null
-  then
-    #setup NERDTree
-    mkdir -p "$HOME/.vim/pack/vendor/start/nerdtree"  && 
-      rm -rf "$HOME/.vim/pack/vendor/start/nerdtree" && 
-      git clone --depth 1 -q https://github.com/preservim/nerdtree.git $HOME/.vim/pack/vendor/start/nerdtree || 
-      echo "Failed to get nerdtree"
+    if command -v git > /dev/null
+    then
+        #setup NERDTree
+        RestorePlugin "nerdtree" "https://github.com/preservim/nerdtree.git"
 
-    [ -f /etc/redhat-release ] && 
-      echo "Installing NerdTree for RHEL" && 
-      mkdir -p "$HOME/.vim/plugins/nerdtree" && 
-      rm -rf "$HOME/.vim/plugins/nerdtree" && 
-      cp -r "$HOME/.vim/pack/vendor/start/nerdtree" "$HOME/.vim/plugins/nerdtree"
-
-    #setup uncrustify
-    if command -v uncrustify > /dev/null ; then
-      mkdir -p "$HOME/.vim/pack/vendor/start/uncrustify" && 
-        rm -rf "$HOME/.vim/pack/vendor/start/uncrustify" && 
-        git clone --depth 1 -q https://github.com/cofyc/vim-uncrustify.git $HOME/.vim/pack/vendor/start/uncrustify || 
-        echo "Failed to get uncrustify"
-
-      [ -f /etc/redhat-release ] && 
-        echo "Installing uncrustify for RHEL" && 
-        mkdir -p "$HOME/.vim/plugins/uncrustify" && 
-        rm -rf "$HOME/.vim/plugins/uncrustify" && 
-        cp -r "$HOME/.vim/pack/vendor/start/uncrustify" "$HOME/.vim/plugins/uncrustify" 
-            else
-              echo "No uncrustify"
+        #setup uncrustify
+        if command -v uncrustify > /dev/null ; then
+            RestorePlugin "uncrustify" "https://github.com/cofyc/vim-uncrustify.git" 
+        else
+            echo "No uncrustify"
+        fi
+        #setup Doxygen
+        if command -v doxygen > /dev/null ; then 
+            RestorePlugin "doxygen" "https://github.com/vim-scripts/DoxygenToolkit.vim.git"
+        else
+            echo "No doxygen"
+        fi
     fi
-  fi
 else
-  echo "No vim"
+    echo "No vim"
 fi
 
 #neovim
 if command -v nvim > /dev/null
 then
-  echo "Restoring NVIM settings"
+    echo "Restoring NVIM settings"
 
-  mkdir -p ~/.config/nvim
-  cp ~/config/nvim/* ~/.config/nvim
+    mkdir -p ~/.config/nvim
+    cp ~/config/nvim/* ~/.config/nvim
 
-  #install nvim plugin manager
-  command -v curl > /dev/null && curl -s -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  echo "make sure to call :PlugInstall after running nvim for the first time"
+    #install nvim plugin manager
+    command -v curl > /dev/null && curl -s -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "make sure to call :PlugInstall after running nvim for the first time"
 else
     echo "No NVIM"
 fi
