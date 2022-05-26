4517924ce199b8cf0a25c71b36bf4b9744da349f
Paul Warnes <paul.d.warnes.ctr@army.mil>
2022-05-26 08:14:13 -0500

    tweaks for multiple versions

--- shortcuts/rh/connectvpn.sh
+++ shortcuts/rh/connectvpn.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+if command -v netExtender &> /dev/null
+then
 netExtender 64.89.105.130:4433 -u warnesp -d localdomain
+elif [ -f /opt/forticlient/vpn ] ;
+then
+sudo /opt/forticlient/vpn --server=https://arl-gateway.axientcorp.com:443/jira-users --user=warnesp --password
+else
+    echo "No VPN found"
+fi
+
 
--- shortcuts/rh/launch-eclipse.sh
+++ shortcuts/rh/launch-eclipse.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-$HOME/eclipse/java-2021-12/eclipse/eclipse
+#$HOME/eclipse/java-2021-12/eclipse/eclipse
+#$HOME/eclipse/java-2022-03/eclipse/eclipse
+
+#looks in the home directory for eclispe installs, run the newest
+
+ $(find "$HOME/eclipse/" -name eclipse -executable -type f | sort -r | head -n 1)
+
