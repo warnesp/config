cbf08bbcec060e7873be7f6a6f3dbf2c0d4ea200
Paul Warnes <pwarnes@gmail.com>
2022-11-20 14:15:22 -0600

    check for cmake before running commands

--- nvim/lua/core/cpp.lua
+++ nvim/lua/core/cpp.lua
@@ -2,10 +2,11 @@
 -- Helpful C++ Stuff --
 -----------------------
 
+local checkCMake = 'if [ -f "CMakeLists.txt" ] ; then echo "In CMake project."; else echo "Not in CMake project." ; exit 0; fi;'
 
 -- build project
-local buildFolder = HOME .. '/build/$(basename $PWD)'
-local savePwd = 'PROD_DIR=$PWD'
+local buildFolder = HOME .. '/build/$(basename "$PWD")'
+local savePwd = 'PROD_DIR="$PWD";'
 local makedir = 'mkdir -p "' .. buildFolder .. '"'
 local chdir = 'cd "' .. buildFolder .. '"'
 local cdBuildDir = makedir .. ' && ' .. chdir
@@ -13,18 +14,18 @@ local linkCompileCommands = 'ln -sf "' .. buildFolder .. '/compile_commands.json
 local cmakeCmd = 'cmake "$PROD_DIR" -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_INSTALL_PREFIX="' .. HOME .. '/.local/"'
 local makeCmd = 'make -j8'
 
-vim.api.nvim_create_user_command('CMake', 'term ' .. savePwd ..'; ' .. cdBuildDir .. ' && ' .. cmakeCmd ..' && ' .. linkCompileCommands .. ' && ' .. makeCmd, {})
+vim.api.nvim_create_user_command('CMake', 'term ' .. savePwd .. checkCMake .. cdBuildDir .. ' && ' .. cmakeCmd ..' && ' .. linkCompileCommands .. ' && ' .. makeCmd, {})
 
 -- clean project
 vim.api.nvim_create_user_command('CleanBuild', '!rm -rf "' .. buildFolder .. '"', {})
 
 -- run tests
-vim.api.nvim_create_user_command('RunTest', 'term ctest --test-dir "' .. buildFolder .. '"', {})
+vim.api.nvim_create_user_command('RunTest', 'term ' .. checkCMake .. ' ctest --test-dir "' .. buildFolder .. '"', {})
 
 
 -- CTags
 local ctagsFolder = HOME .. '/.config/tags/project'
-local ctagsCmd = 'ctags -R --sort=yes --c++-kinds=+p --fields=+iaS -f ' .. ctagsFolder .. '/$(basename $PWD) --extras=+q $PWD'
+local ctagsCmd = 'ctags -R --sort=yes --c++-kinds=+p --fields=+iaS -f ' .. ctagsFolder .. '/$(basename "$PWD") --extras=+q "$PWD"'
 local createCtagsFolder = 'mkdir -p ' .. ctagsFolder .. ';'
 vim.api.nvim_create_user_command('CreateCtags', ':!' .. createCtagsFolder .. ctagsCmd, {})
 
