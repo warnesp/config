897f32a68618d44a67e4bc3ca17fc4cf08d0dc6c
Paul Warnes <paul.d.warnes.ctr@army.mil>
2022-12-14 10:51:12 -0600

    add shellcheck parameter for sourcing files

--- nvim/lua/plugins/syntastic.lua
+++ nvim/lua/plugins/syntastic.lua
@@ -14,3 +14,5 @@ vim.g.syntastic_always_populate_loc_list = 1
 vim.g.syntastic_auto_loc_list = 1
 vim.g.syntastic_check_on_open = 0
 vim.g.syntastic_check_on_wq = 0
+vim.g.syntastic_sh_shellcheck_args = '-x '
+
