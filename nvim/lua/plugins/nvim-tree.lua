455deb401c8b1589033cad39787c6e685fb31c39
Paul Warnes <pwarnes@gmail.com>
2023-09-08 21:12:54 -0500

    remove old config value

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -21,8 +21,6 @@ require("nvim-tree").setup({
     sort_by = "case_sensitive",
     view = {
         adaptive_size = true,
-        mappings = {
-        },
     },
     renderer = {
         group_empty = true,
