a14b7e6dd011b38291f7998afd7014c4422bc12a
Paul Warnes <pwarnes@gmail.com>
2024-03-06 10:34:12 -0600

    basic emacs config

---
+++ emacs/init.el
@@ -0,0 +1,35 @@
+
+(require 'package)
+(require 'org)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-archives
+   '(("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+ '(package-selected-packages nil)
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(org-babel-load-file 
+  (expand-file-name "settings.org" 
+                    user-emacs-directory))
+
+
+
+
+
+
+
+
+
