df4d9e19fd2fde54e3d4e950fcd1c2c43db1cb8d
Paul Warnes <pwarnes@gmail.com>
2025-01-02 22:07:11 -0600

    update leader style

--- nvim/lua/plugins/leader.lua
+++ nvim/lua/plugins/leader.lua
@@ -5,41 +5,35 @@
 --vim.g.leaderMapperWidth = 40
 --vim.g.leaderMapperPos = 'bottom'
 local wk = require("which-key")
+wk.add({
+    {"<leader>p", ":NvimTreeToggle<CR>", desc = "File Tree"},
+    {"<leader>n", ":set number!<CR>", desc = "Line Numbers"},
+    {"<leader>N", ":set relativenumber!<CR>", desc = "Relative Line Numbers"},
+    {"<leader>b", ":GitBlameToggle<CR>", desc = "Toggle git blame"},
+    {"<leader>c", group = "C++"},
+    {"<leader>cb", ":CMake<CR>", desc = "Make project (CMake)"},
+    {"<leader>cB", ":CleanBuild<CR>", desc = "Clean Project"},
+    {"<leader>cd", ":Dox<CR>", desc = "Add doxygen comment"},
+    {"<leader>cD", ":RunTest<CR>", desc = "Run Unit Test"},
+    {"<leader>cn", "<c-]>", desc = "Go to definition Ctrl+]"},
+    {"<leader>cN", "<c-t>", desc = "Go back to previous tag Ctrl-t"},
+    {"<leader>cs", ":FSHere<CR>", desc = "Switch files"},
+    {"<leader>ct", ":Tagbar<CR>", desc = "Tagbar"},
+    {"<leader>cT", ":CreateCtags<CR>", desc = "Create CTags"},
 
-wk.register({
-  p = { ":NvimTreeToggle<CR>", "File Tree" }, -- create a binding with label
-  l = {
-      name = "Lisp",
-      l = {":call vlime#plugin#LoadFile(expand(\"%:p\"))<CR>", "Load File"},
-      n = {":bo vs tmp-repl<CR>|:set filetype=lisp<CR>|:setlocal buftype=nofile<CR>|:setlocal bufhidden=hide<CR>|:setlocal noswapfile<CR>", "Temp REPL"}, 
-      r = {":call vlime#server#New(v:true, get(g:, \"vlime_cl_use_terminal\", v:false))<CR>" ,"Start Server"},
-      R = {":call vlime#plugin#StopCurrentServer()<CR>" ,"Stop Server"},
-  },
-  n = { ":set number!<CR>", "Line Numbers" },
-  N = { ":set relativenumber!<CR>", "Relative Line Numbers" },
-  b = { ":GitBlameToggle<CR>", "Toggle git blame" },
-  s = {
-    name = "Search", -- optional group name
-    s = {":BLines<CR>", "Text search in current buffer"},
-    f = {":GitFiles<CR>", "File Search" },
-    S = {":Lines<CR>", "Text search across all buffers"},
-    g = {":BCommits<CR>", "Search git commits of current buffer"},
-    G = {":Commits<CR>", "Search git commits of repository"},
-    t = {":Tags<CR>", "Search tags"},
-  },
-  c = {
-      name = "C++",
-      b = { ":CMake<CR>", "Make project (CMake)" },
-      B = { ":CleanBuild<CR>", "Clean Project" },
-      d = { ":Dox<CR>", "Add doxygen comment" },
-      d = { ":RunTest<CR>", "Run Unit Test" },
-      n = { "<c-]>", "Go to definition Ctrl-]" },
-      N = { "<c-t>", "Go back to previous tag Ctrl-t" },
-      s = { ":FSHere<CR>", "Switch files" },
-      t = { ":Tagbar<CR>", "Tagbar" },
-      T = { ":CreateCtags<CR>", "Create CTags" },
-  },
-}, { prefix = "<leader>", nowait = true })
+    {"<leader>l", group = "Lisp"},
+    {"<leader>ll", ":call vlime#plugin#LoadFile(expand(\"%:p\"))<CR>", desc = "Load File"},
+    {"<leader>ln", ":bo vs tmp-repl<CR>|:set filetype=lisp<CR>|:setlocal buftype=nofile<CR>|:setlocal bufhidden=hide<CR>|:setlocal noswapfile<CR>", desc = "Temp REPL"}, 
+    {"<leader>lr" ,":call vlime#server#New(v:true, get(g:, \"vlime_cl_use_terminal\", v:false))<CR>" , desc = "Start Server"},
+    {"<leader>lR", ":call vlime#plugin#StopCurrentServer()<CR>" , desc = "Stop Server"},
+    {"<leader>s", group = "Search"},
+    {"<leader>ss", ":BLines<CR>", desc = "Text search in current buffer"},
+    {"<leader>sf", ":GitFiles<CR>", desc = "File Search"},
+    {"<leader>sS", ":Lines<CR>", desc = "Text search across all buffers"},
+    {"<leader>sg", ":BCommits<CR>", desc = "Search git commits of current buffer"},
+    {"<leader>sG", ":Commits<CR>", desc = "Search git commits of repository"},
+    {"<leader>st", ":Tags<CR>", desc = "Search tags"},
+})
 
 
 
