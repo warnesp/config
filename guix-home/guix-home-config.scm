4ea6dd6a6759bc8131c2ad7227d8dedd547216c7
Paul Warnes <pwarnes@gmail.com>
2025-01-04 20:29:48 -0600

    working profile

--- guix-home/guix-home-config.scm
+++ guix-home/guix-home-config.scm
@@ -13,42 +13,45 @@
 (define bash-env 
   '(("EDITOR" . "nvim")
     ;("XDG_DATA_DIRS" . "$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share")
-    ("GUIX_PROFILE" . "$HOME/.guix-profile")))
+    ;("GUIX_PROFILE" . "$HOME/.guix-profile")
+    ))
 
 (define bash-aliases 
   '(("vi" . "nvim")
     ("vim" . "nvim")))
 
 (define bashrc-files
-  ;(list (plain-file "fzf-bindings" "source $HOME/scripts/key-bindings.bash"))
+  (list (plain-file "fzf-bindings" "source $HOME/scripts/key-bindings.bash"))
 
   )
 
 (define home-config
   (home-environment
     (packages (specifications->packages
-		(list 
-		  "bash"
-		  "emacs" 
-		  "fastfetch"
-		  "git" 
-		  "neovim")))
+                (list 
+                  "bash"
+                  "emacs" 
+                  "fastfetch"
+                  "git" 
+                  "neovim"
+                  "make"
+                  )))
     (services
       (list
         (service home-bash-service-type
-		 (home-bash-configuration
-		   (environment-variables bash-env)
-		   ;(bashrc bashrc-files)
-		   (aliases bash-aliases)))
-	(service home-dbus-service-type)
-	(service home-pipewire-service-type)
+                 (home-bash-configuration
+                   (environment-variables bash-env)
+                   (bashrc bashrc-files)
+                   (aliases bash-aliases)))
+        (service home-dbus-service-type)
+        (service home-pipewire-service-type)
 
         (service home-files-service-type
-         `((".guile" ,%default-dotguile)
-           (".Xdefaults" ,%default-xdefaults)))
-
-        (service home-xdg-configuration-files-service-type
+                 `((".guile" ,%default-dotguile)
+                   (".Xdefaults" ,%default-xdefaults)))
+    (service home-xdg-configuration-files-service-type
          `(("gdb/gdbinit" ,%default-gdbinit)
-           ("nano/nanorc" ,%default-nanorc)))))))
+           ("nano/nanorc" ,%default-nanorc)))
+                   ))))
 
 home-config
